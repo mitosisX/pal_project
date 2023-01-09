a1880553476b80c970636f17da7797d07ccec700
mikeChangazi <changazi.mike@gmail.com>
2023-01-09 18:15:35 +0200

    k

--- resources/views/driver/completed/index.blade.php
+++ resources/views/driver/completed/index.blade.php
@@ -8,13 +8,13 @@
             <div class="card">
                 <h3 class="card-header fw-bold text-muted">completed delivery Jobs</h3>
                 <div class="card-body">
-                   <div class="tables-responsive">
+                   <div class="tables-responsive shadow-4">
                    @if(\Session::has('success'))
                    <div class="alert alert-success">
                     <h4>{{\Session::get('success')}}</h4>
                    </div>
                    @endif
-                    <table id="example" class="table table-striped data-table fw-bold text-muted" style="width:100%">
+                    <table id="example" class="table align-middle data-table fw-bold text-muted" style="width:100%">
                         <thead>
                             <tr>
                               
